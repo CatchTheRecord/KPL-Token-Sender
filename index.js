24fc43513b897f1031df7800b4323ebd1ef599dd
CatchTheRecord <110290487+CatchTheRecord@users.noreply.github.com>
2024-09-21 00:28:25 +0200

    initial code parameters

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const transferKPL = require('./transferKPL');
 
-// Функция для массовой рассылки
+// mass send function
 async function massTransfer(mintToken, walletsAndAmounts) {
     for (const { wallet, amount } of walletsAndAmounts) {
         console.log(`Starting transfer to ${wallet} for amount: ${amount}`);
@@ -8,15 +8,15 @@ async function massTransfer(mintToken, walletsAndAmounts) {
     }
 }
 
-// Пример вызова massTransfer с массивом адресов кошельков и сумм
+// massTransfer. Write your address massive
 const walletsAndAmounts = [
-    { wallet: 'CEb8NE6YZdUZPW22gGRJjHupYoQg3P7aAByr6HEebKNg', amount: 1 },
-    { wallet: '5LHzPrir7gkv69TwjkRb7EirFySAx9yUxcV6atbWu2xd', amount: 2 },
-    { wallet: 'FFvoL7yC2fcqd1YtYT5Ng2mkWvbbJgGBnCoBqnkcayzD', amount: 0.5 },
-    // Добавьте столько кошельков, сколько вам нужно
+    { wallet: '', amount: 1 },
+    { wallet: '', amount: 2 },
+    { wallet: '', amount: 0.5 },
+    // type any amount of wallets
 ];
 
-const mintToken = 'n3Rep7GRh3jgkGXaULNu8WzfuwCC9Rcrv82mxzJMnnH'; // Замените на ваш адрес токена
+const mintToken = ''; // your mint KPL address
 
-// Запускаем массовую рассылку
+// start mass send
 massTransfer(mintToken, walletsAndAmounts);
